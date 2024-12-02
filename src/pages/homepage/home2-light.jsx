2ce69076b5a085a0ca6b0eb094c3040b1a8b55e2
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-12-02 09:47:30 +0530

    Icon Cloud

--- src/pages/homepage/home2-light.jsx
+++ src/pages/homepage/home2-light.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar/navbar";
 import Intro2 from "../../components/Intro2/intro2";
+import IconCloud from "../../components/IconCloud/IconCloud";
 import Services from "../../components/Services/services";
 import Video2 from "../../components/Video2/video2";
 import CallToAction from "../../components/Call-to-action/call-to-action";
@@ -10,7 +11,6 @@ import Portfolio from "../../components/Portfolio/portfolio";
 import FullTestimonials from "../../components/Full-testimonials/full-testimonials";
 import Team2 from "../../components/Team2/team2";
 import Blogs2 from "../../components/blogs/Blogs2/blogs2";
-import IconCloud from "../../components/Interactive-icon/IconCloud";
 
 const Homepage2 = () => {
   const navbarRef = React.useRef(null);
