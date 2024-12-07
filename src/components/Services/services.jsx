af8d0dc210fa4da6c8f0827578c3ea12dca9eec1
“Dhruvisha <dhruvisha.gediya143@gmail.com>
2024-12-07 16:47:59 +0530

    change title in service

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -16,7 +16,7 @@ const Services = ({ style, lines }) => {
                 Best Features
               </h6>
               <h3 className="wow color-font">
-                We are a new digital product development agency
+                We are a software Development company
               </h3>
             </div>
           </div>
