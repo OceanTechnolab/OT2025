164f6847ac0ca14bee9565ead687a3d294f96b87
Spatel <shanibpatel@outlook.com>
2024-12-08 02:46:29 +0530

    upgraded to new loader

--- next.config.js
+++ next.config.js
@@ -12,4 +12,28 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass'),
+          },
+        },
+      ],
+      include: path.resolve(__dirname, 'src'),
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      include: path.resolve(__dirname, 'node_modules'),
+    });
+
+    return config;
+  },
 };
