92e210f20ca8480cf5c406c74651ef33d1acc1f3
Sagar <maniyasagar26@gmail.com>
2024-12-25 23:28:44 +1100

    Updates

---
+++ _next/static/chunks/pages/404-687dc9a458d24857.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2336],{94262:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return n(90099)}])},90099:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>s});var u=n(74848);n(96540);let s=()=>(0,u.jsx)("h1",{children:"404 - Page Not Found"})}},_=>{var e=e=>_(_.s=e);_.O(0,[636,6593,8792],()=>e(94262)),_N_E=_.O()}]);
