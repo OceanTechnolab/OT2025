c850b151277524a6db16e534ce49dc49c139c325
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-12-28 00:30:31 +0530

    Added Portfolio Card at HomePage with Functionality

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -38,35 +38,48 @@ const Portfolio = ({ grid, filterPosition }) => {
           >
             <div className="filter">
               <span data-filter="*" className="active">
-                All
+                All Templates
               </span>
-              <span data-filter=".brand">Branding</span>
-              <span data-filter=".web">Mobile App</span>
-              <span data-filter=".graphic">Creative</span>
+              <span data-filter=".brand">Business</span>
+              <span data-filter=".web">Ecommerce</span>
+              <span data-filter=".graphic">Services</span>
+              <span data-filter=".brand">Creative</span>
+              <span data-filter=".web">Blog&Magzine</span>
+              <span data-filter=".graphic">Food</span>
+              <span data-filter=".graphic">Beauty&Health</span>
             </div>
           </div>
 
           <div className="gallery full-width">
+            {/*Basic 6*/}
             <div
               className={`${
                 grid === 3
                   ? "col-lg-4 col-md-6"
                   : grid === 2
-                  ? "col-md-6 lg-mr"
+                  ? "col-md-6"
                   : "col-12"
               } items graphic wow fadeInUp`}
-              data-wow-delay=".1s"
+              data-wow-delay=".4s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/1.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://takshinfinity.com/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/ActionFigureCollection.webp"
+                    alt="Taksh Infinity"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>Creativity Demand</h6>
+                <h6>Taksh Infinity</h6>
                 <span>
-                  <Link href="/works/works-dark">Design</Link>, <Link href="/works/works-dark">WordPress</Link>
+                  <Link href="/works3/works3-dark">Taksh Infinity</Link>,
+                  <Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
@@ -81,16 +94,24 @@ const Portfolio = ({ grid, filterPosition }) => {
               } items web wow fadeInUp`}
               data-wow-delay=".1s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/2.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://mightythread.com/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/ClothingSite2.webp"
+                    alt="image"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>Through The Breaking</h6>
+                <h6>Clothing Site</h6>
                 <span>
-                  <Link href="#0">Design</Link>, <Link href="#0">WordPress</Link>
+                  <Link href="/works3/works3-dark">Clothing Site</Link>,
+                  <Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
@@ -105,16 +126,24 @@ const Portfolio = ({ grid, filterPosition }) => {
               } items brand wow fadeInUp`}
               data-wow-delay=".1s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/3.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://smallworldimmigration.ca/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/ImmigrationServices3.webp"
+                    alt="image"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>Create With Creatives</h6>
+                <h6>Immigration Services</h6>
                 <span>
-                  <Link href="#0">Design</Link>, <Link href="#0">WordPress</Link>
+                  <Link href="/works3/works3-dark">Immigration Services</Link>,
+                  <Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
@@ -129,16 +158,24 @@ const Portfolio = ({ grid, filterPosition }) => {
               } items graphic wow fadeInUp`}
               data-wow-delay=".1s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/4.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://osseous.in/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/OrthopedicManufacturer4.webp"
+                    alt="image"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>Energies of Love</h6>
+                <h6>Orthopedic Manufacture</h6>
                 <span>
-                  <Link href="#0">Design</Link>, <Link href="#0">WordPress</Link>
+                  <Link href="/works3/works3-dark">Orthopedic Manufacture</Link>
+                  ,<Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
@@ -153,16 +190,24 @@ const Portfolio = ({ grid, filterPosition }) => {
               } items web wow fadeInUp`}
               data-wow-delay=".1s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/5.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://curisls.com/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/PharmaceuticalCompany5.webp"
+                    alt="image"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>See It Yourself</h6>
+                <h6>Pharmaceutical Company</h6>
                 <span>
-                  <Link href="#0">Design</Link>, <Link href="#0">WordPress</Link>
+                  <Link href="/works3/works3-dark">Pharmaceutical Company</Link>
+                  ,<Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
@@ -177,16 +222,24 @@ const Portfolio = ({ grid, filterPosition }) => {
               } items brand wow fadeInUp`}
               data-wow-delay=".1s"
             >
-              <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`} className="imago wow">
-                  <img src="/img/portfolio/portfolio/1/6.jpg" alt="image" />
+              <div className="image-container">
+                <Link
+                  href={`https://codexdrugs.in/`}
+                  className="imago wow"
+                  target="blank"
+                >
+                  <img
+                    src="/img/portfolio/portfolio/WorkCategory/HerbalOnlineStore6.webp"
+                    alt="image"
+                  />
                   <div className="item-img-overlay"></div>
                 </Link>
               </div>
               <div className="cont">
-                <h6>Blast From The Past</h6>
+                <h6>Herbal Products</h6>
                 <span>
-                  <Link href="#0">Design</Link>, <Link href="#0">WordPress</Link>
+                  <Link href="/works3/works3-dark">Herbal Products</Link>,
+                  <Link href="/works3/works3-dark">WordPress</Link>
                 </span>
               </div>
             </div>
